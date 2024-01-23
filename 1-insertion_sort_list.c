0158e3ddd8cd0341cb36650e301d5f2b2bfc7a08
preshux <preshux@gmail.com>
2024-01-23 13:56:58 -0800

    ty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,52 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - sort a list
- * @list: UnSorted Linked list
+ * swap_nodes - swaps two nodes in a doubly linked list.
+ * @head: pointer to a pointer to the head of the linked list.
+ * @node1: pointer to the first node to be swapped.
+ * @node2: pointer to the second node to be swapped.
+ */
+
+void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
+{
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*head = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
+}
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in
+ * ascendind order using the insertion sort algorithm; prints the list
+ * after each time the two nodes are swaped
+ *
+ * @list: a pointer to the head of a doubly-linked list
+ *
+ * Return: nothing
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node;
+	listint_t *current, *insert, *tmp;
 
-	if (list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	node = (*list)->next;
-	while (node)
+
+	for (current = (*list)->next; current != NULL; current = tmp)
 	{
-		while ((node->prev) && (node->prev->n > node->n))
+		tmp = current->next;
+		insert = current->prev;
+		while (insert != NULL && current->n < insert->n)
 		{
-			node = swap_node(node, list);
-			print_list(*list);
+			swap_nodes(list, &insert, current);
+			print_list((const listint_t *)*list);
 		}
-		node = node->next;
 	}
-}
-/**
- *swap_node - swap a node helper
- *@node: node
- *@list: node list
- *Return: return pointer of current node
- */
-
-listint_t *swap_node(listint_t *node, listint_t **list)
-{
-	listint_t *back = node->prev, *current = node;
-	/*NULL, 19, 48, 9, 71, 13, NULL*/
-
-	back->next = current->next;
-	if (current->next)
-		current->next->prev = back;
-	current->next = back;
-	current->prev = back->prev;
-	back->prev = current;
-	if (current->prev)
-		current->prev->next = current;
-	else
-		*list = current;
-	return (current);
 }
